051ed6c0492a46270d67553d45bb6a7040fa715c
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-29 16:31:13 -0300

    Initial commit

---
+++ src/test/java/med/voll/clinica_medica_api/ClinicaMedicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med.voll.clinica_medica_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
