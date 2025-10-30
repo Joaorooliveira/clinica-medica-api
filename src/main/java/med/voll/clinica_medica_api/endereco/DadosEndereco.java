b015f4553baa4e147bbd8cf5c34861cad1f448d0
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-30 17:06:42 -0300

    refactor(medico): adiciona validacoes

--- src/main/java/med/voll/clinica_medica_api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/clinica_medica_api/endereco/DadosEndereco.java
@@ -1,4 +1,23 @@
 package med.voll.clinica_medica_api.endereco;
 
-public record DadosEndereco(String logradouro,String bairro,String cep,String cidade, String uf,String complemento,String numero) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+
+        @NotBlank
+        String logradouro,
+        @NotBlank
+        String bairro,
+        @NotBlank
+        @Pattern(regexp = "\\d{8}") //Para validdar o tamanho do cep ou seja  8 digitos
+        String cep,
+        @NotBlank
+        String cidade,
+        @NotBlank
+        String uf,
+
+        String complemento,
+
+        String numero) {
 }
