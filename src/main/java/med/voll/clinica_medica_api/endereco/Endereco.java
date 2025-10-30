b23f1be4eb95a7677a5063b1464920f56ea90a74
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-30 12:25:45 -0300

    feat(medico): adiciona entidade medico e classe auxiliar endereco

--- src/main/java/med/voll/clinica_medica_api/endereco/Endereco.java
+++ src/main/java/med/voll/clinica_medica_api/endereco/Endereco.java
@@ -1,4 +1,32 @@
 package med.voll.clinica_medica_api.endereco;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String bairro;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cidade = dados.cidade();
+        this.numero = dados.numero();
+        this.uf = dados.uf();
+        this.cep = dados.cep();
+        this.complemento = dados.complemento();
+    }
 }
