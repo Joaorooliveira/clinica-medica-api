91aa656a738876d06a7ec424a0a83a84ca015da7
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-30 20:53:24 -0300

    feat(paciente): adiciona repository de paciente

---
+++ src/main/java/med/voll/clinica_medica_api/paciente/PacienteRepository.java
@@ -0,0 +1,6 @@
+package med.voll.clinica_medica_api.paciente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
