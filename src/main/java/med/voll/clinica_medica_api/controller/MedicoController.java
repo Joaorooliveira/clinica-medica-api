883857fe3952aa24155c4481e0df0fc478814566
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-30 12:29:53 -0300

    feat(medicoController): adiciona metodo de criacao de medico Implementa o endpoint POST /medicos

--- src/main/java/med/voll/clinica_medica_api/controller/MedicoController.java
+++ src/main/java/med/voll/clinica_medica_api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.clinica_medica_api.controller;
 
+import med.voll.clinica_medica_api.endereco.Endereco;
 import med.voll.clinica_medica_api.medico.DadosCadastroMedico;
+import med.voll.clinica_medica_api.medico.Medico;
+import med.voll.clinica_medica_api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){ //O RequestBody pega o corpo da requisicao e guarda na String json
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
