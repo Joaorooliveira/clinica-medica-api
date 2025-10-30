2f0986d8dd691047dd04ae772559893858f14c33
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-30 12:26:44 -0300

    feat(medico): adiciona repository de medico

--- src/main/java/med/voll/clinica_medica_api/medico/MedicoRepository.java
+++ src/main/java/med/voll/clinica_medica_api/medico/MedicoRepository.java
@@ -1,4 +1,8 @@
 package med.voll.clinica_medica_api.medico;
 
-public interface MedicoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
 }
