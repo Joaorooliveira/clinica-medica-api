f1bd590c837f535457409425ba5ad8125ae3a636
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-10-31 15:05:34 -0300

    feat(paciente): adiciona dto de listagem  para paciente

---
+++ src/main/java/med/voll/clinica_medica_api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,9 @@
+package med.voll.clinica_medica_api.paciente;
+
+public record DadosListagemPaciente(String nome, String email,String cpf) {
+
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getNome(),paciente.getEmail(),paciente.getCpf());
+
+    }
+}
