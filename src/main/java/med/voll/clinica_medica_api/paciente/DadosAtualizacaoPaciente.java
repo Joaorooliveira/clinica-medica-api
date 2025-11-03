dd859171a5e12db38601311edae526a95a891ec3
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-03 15:51:20 -0300

    feat(paciente):Adiciona nova dto para atualizar o medico

--- src/main/java/med/voll/clinica_medica_api/paciente/DadosAtualizacaoPaciente.java
+++ src/main/java/med/voll/clinica_medica_api/paciente/DadosAtualizacaoPaciente.java
@@ -1,4 +1,15 @@
 package med.voll.clinica_medica_api.paciente;
 
-public record DadosAtualizacaoPaciente() {
+import jakarta.validation.constraints.NotNull;
+import med.voll.clinica_medica_api.endereco.DadosEndereco;
+import med.voll.clinica_medica_api.endereco.Endereco;
+
+public record DadosAtualizacaoPaciente(
+        @NotNull
+        Long id,
+        String telefone,
+        String nome,
+        DadosEndereco endereco
+
+) {
 }
