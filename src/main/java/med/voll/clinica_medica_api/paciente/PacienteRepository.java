c8af4c0b5752a40b98807d5f11524dbba8dd30e0
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-04 13:06:27 -0300

    feat(paciente-repository): adiciona novo metodo para listar apenas os pacientes ativos

--- src/main/java/med/voll/clinica_medica_api/paciente/PacienteRepository.java
+++ src/main/java/med/voll/clinica_medica_api/paciente/PacienteRepository.java
@@ -1,6 +1,11 @@
 package med.voll.clinica_medica_api.paciente;
 
+import aj.org.objectweb.asm.commons.Remapper;
+import med.voll.clinica_medica_api.medico.Medico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
 }
