7252a66c319d514f892054297003e8bda89ecba1
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-07 23:29:34 -0300

    refactor(medico-controller): adiciona retorno dos endpoints para padrao HTPP

--- src/main/java/med/voll/clinica_medica_api/controller/MedicoController.java
+++ src/main/java/med/voll/clinica_medica_api/controller/MedicoController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -21,34 +23,40 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){ //O RequestBody pega o corpo da requisicao e guarda
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder){ //O RequestBody pega o corpo da requisicao e guarda
+
         //na dto dados do tipo DadosDadastroMedico
-        repository.save(new Medico(dados));
+        var medico = new Medico(dados);
+        repository.save(medico);
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10,sort ={"nome"}) Pageable paginacao){ //Tipo de
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10,sort ={"nome"}) Pageable paginacao){ //Tipo de
         // metodo com instrucoes de paginacao e esse  @PageableDefault(size = 10,sort ={"nome"} e para definir o
         // padrao sem precisar alterar a url entao se n mexer nada la na url por padrao vai ficar assim caso queira
         // definir como vc vai querer la na url ai sobrescreve essa Anotacao
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(page);
         //return repository.findAll(paginacao).map(DadosListagemMedico::new); //retorno dps com o page
         //return repository.findAll(paginacao).stream().map(DadosListagemMedico::new).toList(); retorno antes
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
         var medico =repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
-
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id){
+    public ResponseEntity excluir(@PathVariable Long id){
          var medico =repository.getReferenceById(id);
-        medico.excluir();
+         medico.excluir();
         //repository.deleteById(id); //Deleta pelo ID(Exclui do banco de dados)
+        return ResponseEntity.noContent().build();
     }
 }
