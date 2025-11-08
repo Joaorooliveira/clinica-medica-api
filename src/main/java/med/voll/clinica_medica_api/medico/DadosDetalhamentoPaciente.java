16398477685c2ebf974649ab59b752812a2f868f
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-08 00:21:54 -0300

    feat: adiciona novo dto para detalhar paciente

---
+++ src/main/java/med/voll/clinica_medica_api/medico/DadosDetalhamentoPaciente.java
@@ -0,0 +1,12 @@
+package med.voll.clinica_medica_api.medico;
+
+import med.voll.clinica_medica_api.endereco.Endereco;
+import med.voll.clinica_medica_api.paciente.Paciente;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String telefone,
+                                        String cpf, Endereco endereco) {
+
+    public DadosDetalhamentoPaciente (Paciente paciente){
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+    }
+}
