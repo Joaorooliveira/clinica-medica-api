6b07a878bc36a82be80a15cc29710dd7e6825003
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-10 12:28:25 -0300

    feat: adiciona classe controller para validar login e gerar token

--- src/main/java/med/voll/clinica_medica_api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/clinica_medica_api/controller/AutenticacaoController.java
@@ -1,4 +1,33 @@
 package med.voll.clinica_medica_api.controller;
 
+import jakarta.validation.Valid;
+import med.voll.clinica_medica_api.domain.usuario.DadosAutenticacao;
+import med.voll.clinica_medica_api.domain.usuario.Usuario;
+import med.voll.clinica_medica_api.domain.usuario.UsuarioRepository;
+import med.voll.clinica_medica_api.infra.security.TokenService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class AutenticacaoController {
+
+    private final AuthenticationManager manager;
+    private final TokenService tokenService;
+    public AutenticacaoController(AuthenticationManager manager, TokenService tokenService) {
+        this.manager = manager;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var autentication = manager.authenticate(token);
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) autentication.getPrincipal()));
+    }
 }
