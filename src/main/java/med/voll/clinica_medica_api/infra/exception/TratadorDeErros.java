bae1412483719240242c52188c3b87b6692ce33c
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-10 12:31:57 -0300

    refactor(tratador-erros): atualiza metodo

--- src/main/java/med/voll/clinica_medica_api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/clinica_medica_api/infra/exception/TratadorDeErros.java
@@ -1,4 +1,4 @@
-package med.voll.clinica_medica_api.infra;
+package med.voll.clinica_medica_api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
