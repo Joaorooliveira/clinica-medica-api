47249a94ded6d1c6abc1204f1e06d80abc697293
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-26 22:25:04 -0300

    feat: adiciona entidade consulta

--- src/main/java/med/voll/clinica_medica_api/domain/consulta/Consulta.java
+++ src/main/java/med/voll/clinica_medica_api/domain/consulta/Consulta.java
@@ -1,4 +1,33 @@
 package med.voll.clinica_medica_api.domain.consulta;
 
+import jakarta.persistence.*;
+import lombok.*;
+import med.voll.clinica_medica_api.domain.medico.Medico;
+import med.voll.clinica_medica_api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime data;
 }
