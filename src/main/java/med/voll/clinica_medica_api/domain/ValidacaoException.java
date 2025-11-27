11737a3de12a7e7387e023b9589165b9914a7718
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-27 20:45:21 -0300

    feat: adiciona exception personalizada

---
+++ src/main/java/med/voll/clinica_medica_api/domain/ValidacaoException.java
@@ -0,0 +1,7 @@
+package med.voll.clinica_medica_api.domain;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
