30b3d2015fac1a4172245665760e2d73d317395b
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-27 21:23:39 -0300

    feat: adiciona classe para validar horario de funcionamento

--- src/main/java/med/voll/clinica_medica_api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
+++ src/main/java/med/voll/clinica_medica_api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -1,4 +1,19 @@
 package med.voll.clinica_medica_api.domain.consulta.validacoes;
 
+import med.voll.clinica_medica_api.domain.ValidacaoException;
+import med.voll.clinica_medica_api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+
 public class ValidadorHorarioFuncionamentoClinica {
+    public void validar(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.data();
+
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAberturaDaClinica = dataConsulta.getHour()<7;
+        var depoisDoEncerramentoDaClinica = dataConsulta.getHour() >18;
+        if(domingo || antesDaAberturaDaClinica || depoisDoEncerramentoDaClinica) {
+            throw new ValidacaoException("Consulta fora do horario de funcionamento da clinica");
+        }
+    }
 }
