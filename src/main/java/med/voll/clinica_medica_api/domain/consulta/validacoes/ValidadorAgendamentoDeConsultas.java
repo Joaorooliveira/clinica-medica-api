8dbea8aca5c03a35f0002869bc7fa30c1de87af0
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-29 22:47:10 -0300

    feat: adiciona interface para validar consultas

--- src/main/java/med/voll/clinica_medica_api/domain/consulta/validacoes/ValidadorAgendamentoDeConsultas.java
+++ src/main/java/med/voll/clinica_medica_api/domain/consulta/validacoes/ValidadorAgendamentoDeConsultas.java
@@ -1,4 +1,7 @@
 package med.voll.clinica_medica_api.domain.consulta.validacoes;
 
+import med.voll.clinica_medica_api.domain.consulta.DadosAgendamentoConsulta;
+
 public interface ValidadorAgendamentoDeConsultas {
+     void validar(DadosAgendamentoConsulta dados);
 }
