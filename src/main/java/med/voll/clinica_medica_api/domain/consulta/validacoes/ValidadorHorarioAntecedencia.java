c003c534c9820247ad1195a19c63ce90632a6685
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-29 22:47:48 -0300

    feat: adiciona validador para marcar consulta com antecedencia

--- src/main/java/med/voll/clinica_medica_api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
+++ src/main/java/med/voll/clinica_medica_api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -2,17 +2,19 @@ package med.voll.clinica_medica_api.domain.consulta.validacoes;
 
 import med.voll.clinica_medica_api.domain.ValidacaoException;
 import med.voll.clinica_medica_api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class ValidadorHorarioAntecedencia {
-    public void validar(DadosAgendamentoConsulta dados){
+@Component
+public class ValidadorHorarioAntecedencia implements ValidadorAgendamentoDeConsultas {
+    public void validar(DadosAgendamentoConsulta dados) {
         var dataConsulta = dados.data();
         var agora = LocalDateTime.now();
-        var diferencaEmMinutos = Duration.between(agora,dataConsulta).toMinutes();
+        var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
 
-        if(diferencaEmMinutos <30){
+        if (diferencaEmMinutos < 30) {
             throw new ValidacaoException("Consulta deve ser agendada com antecedencia minima de 30 minutos");
         }
     }
