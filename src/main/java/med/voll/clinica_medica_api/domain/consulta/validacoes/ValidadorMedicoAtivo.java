b9a1c35ff18565e96ca4ab14db56145ccfe66c21
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-29 22:48:21 -0300

    feat": adiciona validador para saber se medico esta ativo

--- src/main/java/med/voll/clinica_medica_api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
+++ src/main/java/med/voll/clinica_medica_api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -1,4 +1,26 @@
 package med.voll.clinica_medica_api.domain.consulta.validacoes;
 
-public class ValidadorMedicoAtivo {
+import med.voll.clinica_medica_api.domain.ValidacaoException;
+import med.voll.clinica_medica_api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.clinica_medica_api.domain.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorMedicoAtivo implements ValidadorAgendamentoDeConsultas {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        //escolha do medico opcional
+        if (dados.idMedico() == null) {
+            return;
+        }
+
+        var medicoEstaAtivo = repository.findAtivoById(dados.idMedico());
+        if (!medicoEstaAtivo) {
+            throw new ValidacaoException("Consulta não pode ser agendada com medico excluido");
+        }
+    }
 }
