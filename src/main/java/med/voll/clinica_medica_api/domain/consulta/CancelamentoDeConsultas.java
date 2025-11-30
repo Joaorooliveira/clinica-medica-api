60b8f405bb56d11028b78c7c6a0b8104c2d5b5c7
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-29 22:57:16 -0300

    feat: adiciona dto para dados de cancelamento da consulta

--- src/main/java/med/voll/clinica_medica_api/domain/consulta/CancelamentoDeConsultas.java
+++ src/main/java/med/voll/clinica_medica_api/domain/consulta/CancelamentoDeConsultas.java
@@ -1,4 +1,24 @@
 package med.voll.clinica_medica_api.domain.consulta;
 
+import jakarta.persistence.EntityNotFoundException;
+import med.voll.clinica_medica_api.domain.consulta.validacoes.ValidadorCancelamentoAntecedencia;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CancelamentoDeConsultas {
+
+    private final ConsultaRepository repository;
+    private final ValidadorCancelamentoAntecedencia validador;
+
+    public CancelamentoDeConsultas(ConsultaRepository repository, ValidadorCancelamentoAntecedencia validador) {
+        this.repository = repository;
+        this.validador = validador;
+    }
+
+
+    public void CancelamentoDeConsultas(DadosCancelamentoConsulta dados) {
+        var consulta = repository.findById(dados.idConsulta()).orElseThrow(()-> new EntityNotFoundException(
+                "ID da consulta errada, ou consulta nao existe"));
+    validador.validar(dados);
+    }
 }
--- src/main/java/med/voll/clinica_medica_api/domain/consulta/DadosCancelamentoConsulta.java
+++ src/main/java/med/voll/clinica_medica_api/domain/consulta/DadosCancelamentoConsulta.java
@@ -1,4 +1,12 @@
 package med.voll.clinica_medica_api.domain.consulta;
 
-public record DadosCancelamentoConsulta() {
+import jakarta.validation.constraints.NotNull;
+
+public record DadosCancelamentoConsulta(
+        @NotNull
+        Long idConsulta,
+
+        @NotNull
+        MotivoCancelamento motivoCancelamento
+) {
 }
