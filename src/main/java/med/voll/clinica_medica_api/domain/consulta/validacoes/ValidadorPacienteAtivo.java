22368e000b4ee2a772b6f3968e35bc37a989321e
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-29 22:49:24 -0300

    feat: adiciona validador para saber se paciente esta ativo

--- src/main/java/med/voll/clinica_medica_api/domain/consulta/validacoes/ValidadorPacienteAtivo.java
+++ src/main/java/med/voll/clinica_medica_api/domain/consulta/validacoes/ValidadorPacienteAtivo.java
@@ -1,4 +1,21 @@
 package med.voll.clinica_medica_api.domain.consulta.validacoes;
 
-public class ValidadorPacienteAtivo {
+import med.voll.clinica_medica_api.domain.ValidacaoException;
+import med.voll.clinica_medica_api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.clinica_medica_api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorPacienteAtivo implements ValidadorAgendamentoDeConsultas {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var pacienteEstaAtivo = repository.findAtivoById(dados.idPaciente());
+        if (!pacienteEstaAtivo) {
+            throw new ValidacaoException("Consulta nao pode ser agendada com paciente excluido");
+        }
+    }
 }
