dbe5c6a1c9f4271ecec2415e899f30933407adc3
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-10 17:01:10 -0300

    chore(properties): adiciona configuracao para mostrar formato sql

---
+++ src/test/java/med/voll/clinica_medica_api/controller/ConsultaControllerTest.java
@@ -0,0 +1,7 @@
+package med.voll.clinica_medica_api.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsultaControllerTest {
+
+}
---
+++ src/test/java/med/voll/clinica_medica_api/controller/MedicoControllerTest.java
@@ -0,0 +1,12 @@
+package med.voll.clinica_medica_api.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedicoControllerTest {
+
+    @Test
+    void cadastrar() {
+    }
+}
